8b541760ffbf6e651582d39c261b21ea8edbdfae
Timothy Sharkey <Techne3@gmail.com>
2019-10-29 12:24:45 -0600

    file clean up

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require('express');
 
 // 1
-
-const hubsRouter = require(`./hubs/hubs-router.js`)
+const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
 server.use(express.json());
 
 server.get('/', (req, res) => {
-  
+  res.send(`
+    <h2>Lambda Hubs API</h>
+    <p>Welcome to the Lambda Hubs API</p>
+  `);
 });
 
-//2
-server.use(`/api/hubs`, hubsRouter);
+// 2
+server.use('/api/hubs', hubsRouter);
 
 server.listen(4000, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
